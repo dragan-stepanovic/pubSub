f4c447107961868649ae5f2733726e6e982f666f
dragan.stepanovic <dragan.stepanovic@nultien.rs>
2017-08-08 22:52:17 +0200

    todo added; recorder formatted

--- PubSub/Recorder.cs
+++ PubSub/Recorder.cs
@@ -3,13 +3,10 @@ using System.Collections.Generic;
 
 namespace PubSub
 {
-    internal class Recorder<T>
-    {
-        public Action<string, T> Handler
-        {
-            get { return (topic, message) => Messages.Add(Tuple.Create(topic, message)); }
-        }
+	internal class Recorder<T>
+	{
+		public Action<string, T> Handler => (topic, message) => Messages.Add(Tuple.Create(topic, message));
 
-        public List<Tuple<string, T>> Messages { get; } = new List<Tuple<string, T>>();
-    }
+		public List<Tuple<string, T>> Messages { get; } = new List<Tuple<string, T>>();
+	}
 }
