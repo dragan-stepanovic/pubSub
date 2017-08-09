82adde3f8d654f3e5c633e47a75ef770a1d2741e
dragan.stepanovic <dragan.stepanovic@nultien.rs>
2017-08-09 10:26:49 +0200

    Topic tests added

--- PubSub/Solution/Topic.cs
+++ PubSub/Solution/Topic.cs
@@ -1,4 +1,7 @@
-﻿namespace PubSub.Solution
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace PubSub.Solution
 {
 	public class Topic
 	{
@@ -6,6 +9,15 @@
 
 		public Topic(string value)
 		{
+			if (string.IsNullOrWhiteSpace(value) || !value.StartsWith("/") || value.Contains("//") || value.EndsWith("/") )
+				throw new InvalidTopicException(value);
+
+			var alphaNumeric = new Regex("^[a-zA-Z0-9]*$");
+			var levels = value.Split('/');
+
+			if (levels.Any(level => !alphaNumeric.IsMatch(level)))
+				throw new InvalidTopicException(value);
+
 			_value = value;
 		}
 
---
+++ PubSub/TopicTests.cs
@@ -0,0 +1,30 @@
+﻿using PubSub.Solution;
+using Xunit;
+
+namespace PubSub
+{
+	public class TopicTests
+	{
+		[Theory]
+		[InlineData(null)]
+		[InlineData("")]
+		[InlineData(" ")]
+		[InlineData("invalid")]
+		[InlineData("invalid/")]
+		[InlineData("/home/")]
+		[InlineData("//home")]
+		[InlineData("/home/#")]
+		public void CannotCreateInvalidTopic(string topicAsString)
+		{
+			var ex = Assert.Throws<InvalidTopicException>(() => new Topic(topicAsString));
+			Assert.NotNull(ex);
+		}
+
+		[Theory]
+		[InlineData("/home/123")]
+		public void CanCreateTopicFromValidString(string topicAsString)
+		{
+			new Topic(topicAsString);
+		}
+	}
+}
