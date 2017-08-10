aa9ec19c09e9289693a6ae22f41a5004b39ff0a5
dragan.stepanovic <dragan.stepanovic@nultien.rs>
2017-08-10 15:53:35 +0200

    equals/gethashcode and ToString implemented

--- PubSub/Solution/Level.cs
+++ PubSub/Solution/Level.cs
@@ -9,9 +9,26 @@ namespace PubSub.Solution
 			_value = asString;
 		}
 
-		public bool Matches(Level thatLevel)
+		//todo: revert back to matches to allign with domain
+		public bool Equals(Level thatLevel)
 		{
 			return _value.Equals(Wildcard.SingleLevel) || _value.Equals(Wildcard.MultiLevel) || this._value.Equals(thatLevel._value);
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			return obj is Level && Equals((Level) obj);
+		}
+
+		public override int GetHashCode()
+		{
+			return _value != null ? _value.GetHashCode() : 0;
+		}
+
+		public override string ToString()
+		{
+			return $"{_value}";
 		}
 	}
 }
