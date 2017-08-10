5a816c678070a7076839e6d8dc06f26adf7caa48
dragan.stepanovic <dragan.stepanovic@nultien.rs>
2017-08-10 15:52:08 +0200

    order of methods changed

--- PubSub/Solution/IPubSub.cs
+++ PubSub/Solution/IPubSub.cs
@@ -9,17 +9,17 @@ namespace PubSub.Solution
 	public interface IPubSub<T>
 	{
 		/// <summary>
-		/// Publishes the message to specified topic.
-		/// </summary>
-		/// <param name="message">The message.</param>
-		/// <param name="topicAsString">The topic.</param>
-		void Publish(string topicAsString, T message);
-
-		/// <summary>
 		/// Subscribes to the specified topic.
 		/// </summary>
 		/// <param name="topicAsString">The topic.</param>
 		/// <param name="callback">The callback.</param>
 		void Subscribe(string topicAsString, Action<string, T> callback);
+
+		/// <summary>
+		/// Publishes the message to specified topic.
+		/// </summary>
+		/// <param name="message">The message.</param>
+		/// <param name="topicAsString">The topic.</param>
+		void Publish(string topicAsString, T message);
 	}
 }
